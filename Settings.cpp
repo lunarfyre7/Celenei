dd787112055029908e40666ca30213f7ffb70202
Jake Vandereay <gvandereay@gmail.com>
2015-09-08 20:42:52 -0700

    ...

--- Settings.cpp
+++ Settings.cpp
@@ -6,14 +6,69 @@
  */
 
 #include "Settings.h"
-using sol::Settings;
+//#include "config.h"
+#include <avr/eeprom.h>
+
+using namespace sol;
+using namespace std;
+
 
 Settings::Settings() {
-	// TODO Auto-generated constructor stub
-
+	Read();
 }
 
 Settings::~Settings() {
-	// TODO Auto-generated destructor stub
 }
+void Settings::Register(char name[], int val) {
+	setting_t sett;
+	sett.val = val;
+	sett.hash = StrHash(name);
+	intlist.push_back(sett);
+	Save();
+}
+void Settings::Set(char name[], int val) {
+	int index = Find(StrHash(name));
+	if (index == -1)
+		Register(name, val);
+	else {
+		intlist[index].val = val;
+		Save();
+	}
+}
+int  Settings::Get(char name[]) {
+	int index = Find(StrHash(name));
+	return intlist[index].val;
+}
+int Settings::Find(int hash) {
+	for(std::vector<setting_t>::iterator it=intlist.begin(); it != intlist.end(); it++)
+		if(it->hash == hash)
+			return std::distance(intlist.begin(), it);
+	return -1;
+}
+int Settings::StrHash(char string[]) {
+	int hash = SEED; //seed
+	int len = sizeof(string)/sizeof(string[0]);
+	for(int i=0;i<len;i++)
+		hash *= (int) string[i];
+	return hash;
+}
+int Settings::IntHash(int num) {
+	int hash = SEED; //seed
+	do {
+		hash *= num && 0x0f;
+		num = num >> 0x0f;
+	} while(num > 0);
+	return hash;
+}
+void Settings::Save() {
+	int pos = 0;
+	uint8_t size = sizeof(int)/sizeof(byte);
+	for(std::vector<setting_t>::iterator it=intlist.begin(); it != intlist.end(); it++) {
+		if(eeprom_read_word((uint16_t*)&pos) != it->val)
+			eeprom_write_word((uint16_t*)&pos, it->val);
+		pos += size;
+	}
+}
+void Settings::Read() {
 
+}
--- Settings.h
+++ Settings.h
@@ -5,19 +5,31 @@
  *      Author: Jake
  */
 #include "config.h"
-//#include <list>
+#include <iterator>
+#include <vector>
 
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 namespace sol { //TODO wrap all core classes in this namespace
+	struct setting_t {
+		int val;
+		int hash;
+	};
 	class Settings {
 	public:
 		Settings();
 		virtual ~Settings();
 
-		void Register(char name[], uint8_t val);
+		void Register(char name[], int val);
+		void Set(char name[], int val);
+		int  Get(char name[]);
+		void Save();
+		void Read();
 	private:
-//		std::list<*>
+		static std::vector<setting_t> intlist;
+		int StrHash(char string[]);
+		int IntHash(int);
+		int Find(int);
 	};
 }
 #endif /* SETTINGS_H_ */
